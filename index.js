6aeb380837aab7fc59ef5a550c7501aea6cb3432
Kunal Pandey <10c.kunalpandey@gmail.com>
2026-03-16 22:35:38 +0530

    Added tfScorer

--- index.js
+++ index.js
@@ -1,68 +1,58 @@
 import fs from "fs"
 import Clean from "./stemming.js";
-import { inflateRaw } from "zlib";
+import makeItf from "./tfScorer.js";
 
 const Word_Set = new Set();
 
 const AllFilesWords = [];
 
-const allFileData = JSON.parse(fs.readFileSync("./allFileData.json","utf-8"));
-
 const files = fs.readdirSync("./DOCS");
 
-const FILES_INCLUDED = JSON.parse(fs.readFileSync("./filesIncluded.json","utf-8"));
+const FILES_INCLUDED = JSON.parse(fs.readFileSync("./filesIncluded.json", "utf-8"));
 
 for (const filename of files) {
     if (FILES_INCLUDED.includes(filename)) {
         continue;
     } else {
-
+        const allWordsOfFile = []
         const readDocBuf = fs.readFileSync(`./DOCS/${filename}`);
         const DocContent = String(readDocBuf);
         const AllWords = DocContent.split(" ")
         // console.log(AllWords)
         for (const word of AllWords) {
             const WORD = Clean(word);
-            AllFilesWords.push(WORD);
+            allWordsOfFile.push(WORD);
             // Word_Set.add(WORD)
         }
-        for(const word of AllFilesWords){
-            Word_Set.add(word);
-        }
+        AllFilesWords.push(allWordsOfFile)
         FILES_INCLUDED.push(filename);
     }
 }
 
-// todo -> rn all words are considered acroos all files, the correct thing would be all words mapped to their frequency of the current file's words
-
-for (const filename of files) {
-    if(!allFileData.includes(filename)){
-            const fileobj = {
-                totalWords : AllFilesWords.length,
-            };
-            for(const ref_word of AllFilesWords){
-                let count = 0;
-            for(const word of AllFilesWords){
-                if(ref_word===word){
-                        count++;
-                        fileobj[word] = count;
-                    }
-            }
-            }
-
-            allFileData.push([filename,fileobj])
-}
+for (const filewordset of AllFilesWords) {
+    for (const word of filewordset) {
+        Word_Set.add(word);
+    }
 }
 
-fs.writeFileSync("./filesIncluded.json",JSON.stringify(FILES_INCLUDED,null,2))
-fs.writeFileSync('./allFileData.json',JSON.stringify(allFileData,null,2));
+fs.writeFileSync("./filesIncluded.json", JSON.stringify(FILES_INCLUDED, null, 2))
 
-const content = ["",...Word_Set].join('\n')
+const content = [...Word_Set].join('\n')
 
-const stream = fs.createWriteStream("./Words.txt",{
-    flags:"a+"
-});
-for (const word of content) {
-    stream.write(word);
+
+// async method hence had to make sync
+// const stream = fs.createWriteStream("./Words.txt", {
+//     flags: "a+"
+// });
+// for (const word of content) {
+//     stream.write(word);
+// }
+// stream.close();
+
+fs.writeFileSync("./Words.txt",content,{
+    flag:"a+",
+})
+
+for(const file of files){
+    makeItf(file);
 }
-stream.close();
---
+++ tfScorer.js
@@ -0,0 +1,52 @@
+import fs from "fs"
+import Clean from "./stemming.js";
+
+export default async function makeItf(filename){
+    const allWordsRaw = String(fs.readFileSync(`./Words.txt`));
+    const allWordsCleaning = allWordsRaw.split("\n");
+    const allWordsCleanedSet = new Set();
+    for(const word  of allWordsCleaning){
+        allWordsCleanedSet.add(Clean(word));
+    }
+    const allWordsCleaned = [...allWordsCleanedSet]
+    // console.log(allWordsCleaned)
+    let allFileData = JSON.parse(fs.readFileSync("./allFileData.json","utf-8"));
+    let flag = false;
+    for(const filedata of allFileData){
+        if(filedata['filename']===filename){
+            flag  = true;
+        }
+    }
+    let fileDataObject;
+    if(flag){
+        return;
+    }else{
+        fileDataObject = {
+        "filename":filename,
+        }
+    }
+    let fileWords = fs.readFileSync(`./DOCS/${filename}`,"utf-8").split(" ");
+    const cleanedFileWords = [];
+    for(const word of fileWords){
+        cleanedFileWords.push(Clean(word))
+    }
+    fileDataObject["totalVocab"] = cleanedFileWords.length;
+    for(const ref_word of allWordsCleaned){
+        let count = 0;
+        for(const word of cleanedFileWords){
+            if(ref_word==word){
+                count++;
+            }
+        }
+        fileDataObject[ref_word] = {
+            frequency : count,
+            tf: count/(fileDataObject["totalVocab"]+1)
+        } ;
+    }
+    console.log(fileDataObject)
+
+    allFileData.push(fileDataObject);
+
+    fs.writeFileSync("./allFileData.json",JSON.stringify(allFileData,null,2))
+
+}
